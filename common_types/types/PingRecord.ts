271e7d019d7514da61680d8089a8806bd5f785e8
Simon Howey <simon.howey@gmail.com>
2017-10-05 14:15:31 -0700

    small changes to ping record type

--- common_types/types/PingRecord.ts
+++ common_types/types/PingRecord.ts
@@ -1,7 +1,6 @@
 export interface PingRecord {
     ping_recid: number;
     device_id: number;
-    ip_address: string;
     ms_response: number;
     responded: boolean;
     datetime: Date;
--- utils/data_faker/dataFaker.ts
+++ utils/data_faker/dataFaker.ts
@@ -2,26 +2,6 @@
 
 export class DataFaker {
 
-    // Company(): Company {
-    //     return {
-    //         recid: 1,
-    //         id: "fakeid",
-    //         name: "fakename"
-    //     };
-    // }
 
-    // Site(): Site {
-    //     return {
-    //         recid: 1,
-    //         company_recid: 1,
-    //         description: "fakesite",
-    //         address1: "fakeaddress1",
-    //         address2: "fakeaddress2",
-    //         city: "fakecity",
-    //         province: "fakeprovince",
-    //         postal_code: "fakepostalcode",
-    //         latitude: "5",
-    //         longitude: "6"
-    //     }
-    // }
+
 }
