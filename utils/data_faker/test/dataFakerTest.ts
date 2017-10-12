62254772cca9815d6e51487f91272093426ecb40
Sawmawn Golestani <sawmawn.golestani@gmail.com>
2017-10-12 13:00:55 -0700

    Created dataFaker class

---
+++ utils/data_faker/test/dataFakerTest.ts
@@ -0,0 +1,15 @@
+import {expect} from 'chai';
+import {Company} from "../../../../uniserve.m8s/common_types";
+import {Device} from "../../../../uniserve.m8s/common_types";
+
+
+// describe('dataFakerTest', function() {
+//     const dataFaker = new dataFaker();
+
+//     before(function (done) {
+//         let dataSet: JSON = top10000.JSON;
+//     }
+//     it("test addDataSet", function() {
+//         dataFaker.addDataSet()
+//     });
+// });
