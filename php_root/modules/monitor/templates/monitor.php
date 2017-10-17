42b819271ad9607b5b7b70934c070c74cf7924a6
Simon Howey <simon.howey@gmail.com>
2017-10-17 11:38:58 -0700

    fixing css2

--- php_root/modules/monitor/templates/monitor.php
+++ php_root/modules/monitor/templates/monitor.php
@@ -4,8 +4,7 @@
 ?>
 
 <!-- Bootstrap CSS -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
         crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
@@ -20,7 +19,7 @@
 <link rel="stylesheet" href="./css/style.css">
 
 
-<div id="m8sbody" style="width: 100vw; margin-left:-60px; margin-right: -60px; margin-top: -13px;>
+<div id="m8sbody" style="margin-left:-60px; margin-right: -60px; margin-top: -13px; height: 780px; padding-bottom: 30px;>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
