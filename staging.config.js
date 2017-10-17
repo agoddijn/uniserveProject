343893249cea26e7fff24224d4f9e0013e5d2424
Simon Howey <simon.howey@gmail.com>
2017-10-17 10:31:39 -0700

    update config

--- staging.config.js
+++ staging.config.js
@@ -10,7 +10,7 @@ module.exports = {
         script    : './services/WebBackend/src/WebBackend.js',
         env : {
           "NODE_ENV": 'staging',
-          "PORT"    : 3031,
+          "WEBBACKEND_PORT"    : 3031,
           "PHP_AUTH_TOKEN": "tfFu9iEUfNjmW6Oj3sOSPS4BKGeBKTaJ",
           "DB_HOST" : 'localhost',
           "DB_PORT" : 5432,
@@ -26,7 +26,7 @@ module.exports = {
         script    : './services/PingingBackend/src/PingingBackend.js',
         env : {
           "NODE_ENV": 'staging',
-          "PORT"    : 3031,
+          "WEBBACKEND_PORT"    : 3031,
           "DB_HOST" : 'localhost',
           "DB_PORT" : 5432,
           "DB_NAME" : 'ubc03',
