ecd56f97000ad7d7d98c9a20ff88bb634ad4f811
Spencer Austman <spencerlgaustman@gmail.com>
2017-10-24 15:27:47 -0700

    Added sql script to delete all records within the database

---
+++ database/delete_all_records.sql
@@ -0,0 +1,8 @@
+--- This script will delete all existing records in the ENTIRE datbase ---
+
+DELETE FROM msp_ping_60;
+DELETE FROM msp_ping_30;
+DELETE FROM msp_ping;
+DELETE FROM msp_device;
+DELETE FROM msp_site;
+DELETE FROM msp_company;
