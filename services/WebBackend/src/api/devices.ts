d2928e46f3088d2cab191d5d81b79f2e40c4b153
Simon Howey <simon.howey@gmail.com>
2017-10-24 12:23:41 -0700

    trying to get db interface working

--- services/WebBackend/src/api/devices.ts
+++ services/WebBackend/src/api/devices.ts
@@ -3,39 +3,27 @@ import {HTTPMainPageResponse, Site, Device} from "uniserve.m8s.types";
 import {Log, DbInterface} from "uniserve.m8s.utils";
 
 
-export let devices = (req: Request, res: Response) => {
+export let devices = async (req: Request, res: Response) => {
     //might be a string tbh
-    let request_id = <number> req.params.company_recid;
+    const request_id = <number> parseInt(req.params.company_recid);
     Log.trace("API:devices");
     Log.info("API:devices # company_recid: " + request_id);
 
-    let fakesite: Site = {
-        site_recid: 1,
-        company_recid: request_id,
-        description: "fake site",
-        address1: "fake address1",
-        address2: "fake address2",
-        city: "fake city",
-        province: "fake province",
-        postal_code: "fake postalcode",
-        latitude: "0",
-        longitude: "0" 
-    };
+    try {
+        const db = new DbInterface();
+        const sites: Site[] =  <Site[]> await db.getCompanyDevices(request_id)[0];
+        
+        for(let site of sites){
+            for(let device of site.devices){
+                device.ping_records = await db.getRecentPings(device.device_recid); 
+            }
+        }
 
-    let fakedevice: Device = {
-        device_recid: 1,
-        site_recid: 1,
-        device_id: "fake device_id",
-        manufacturer: "fake manufacturer",
-        description: "fake description",
-        device_type: "fake device type",
-        mac_address: "fake mac address",
-        ip_address: "fake ip",
-        ping_records: [{ping_recid: 1, device_recid: 1, ms_response: 100, responded: true, datetime: new Date()}]
+        res.json(sites);  
+
+    } catch(e) {
+        //TODO proper errors
+        res.json("error");
     }
 
-    fakesite.devices = [fakedevice];
-    let response: HTTPMainPageResponse = {sites: [fakesite]};
-
-    res.json(response);
 }
