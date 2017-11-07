01267d915d75614e0b54dae6a8363ab6c44c845b
Simon Howey <simon.howey@gmail.com>
2017-11-07 12:10:19 -0800

    test

--- staging.config.js
+++ staging.config.js
@@ -34,7 +34,22 @@ module.exports = {
           "DB_PASS" : 'olivepepsi'
         }
       }
-    ]
+    ],
+
+    "deploy" : {
+      "staging" : {
+        "user" : "ubc03",
+        // Multi host is possible, just by passing IPs/hostname as an array
+        "host" : "lab3.uniserve.com",
+        // Branch
+        "ref"  : "origin/staging",
+        // Git repository to clone
+        "repo" : "git@github.com:CPSC319-2017w1/uniserve.m8s.git",
+        // Path of the application on target servers
+        "path" : "~/",
+        // Commands to be executed on the server after the repo has been cloned
+        "post-deploy" : "yarn install && npm run buildstaging && pm2 startOrRestart staging.config.js"
+      },
   
-  };
-  
+  }
+}
