786f8618096fa13274e7c2cb49edf452ab291e60
cpscprojectpy <cpscprojectpy@users.noreply.github.com>
2017-11-13 04:22:54 -0800

    Add DeviceTable to map

--- services/Frontend/src/components/MarkerWrapper.tsx
+++ services/Frontend/src/components/MarkerWrapper.tsx
@@ -3,6 +3,7 @@ import { Site, Device } from "uniserve.m8s.types";
 import { ContainerBar } from "./PresentationalContainerBar"
 import { MapIframeContainer } from "./MapIframeContainer"
 import { DeviceBody } from "./PresentationalDeviceBody"
+import { DeviceTable } from "./DeviceTable"
 // import { NewMapContainer } from "./NewMapContainer"
 const { compose } = require("recompose");
 const {
@@ -19,7 +20,7 @@ export class MarkerWrapper extends React.Component<{ Site: Site, num:number, inf
     }
     render() {
         return <Marker key={this.props.num} position={{ lat: this.props.Site.latitude, lng: this.props.Site.longitude }} onClick={() => {this.setState({clicked:!this.state.clicked})}} >
-                    {this.props.Site.devices.length!==0 && this.state.clicked && <InfoWindow onClick={()=>{this.setState({clicked:!this.state.clicked})}}><div ><DeviceBody Devices={this.props.Site.devices} tabular={false} display={true} num={this.props.num}/></div></InfoWindow>}
+                    {this.props.Site.devices.length!==0 && this.state.clicked && <InfoWindow onClick={()=>{this.setState({clicked:!this.state.clicked})}}><div ><DeviceTable devices={this.props.Site.devices}/></div></InfoWindow>}
                 </Marker>
     }
 }
