a906cff3e693c8b1348583237735330bf6ad2c64
Simon Howey <simon.howey@gmail.com>
2017-11-14 04:27:11 -0800

    fix for length of lat/long going into database

--- services/WebBackend/src/api/devices.ts
+++ services/WebBackend/src/api/devices.ts
@@ -5,12 +5,13 @@ import {DbInterface} from "uniserve.m8s.web.db_interface";
 import {Geocoder} from "../geocoding/Geocoder";
 
 export let devices = async (req: Request, res: Response) => {
-    const geocoder = new Geocoder();
     const request_id = parseInt(req.params.company_recid);
     Log.info("API:devices # company_recid: " + request_id);
 
     try {
         const db = new DbInterface();
+        const geocoder = new Geocoder();
+        
         const siteData = await db.getCompanyDevices(request_id);
         const sites: Site[] = siteData[0].sites;
         
@@ -21,7 +22,7 @@ export let devices = async (req: Request, res: Response) => {
                 site.latitude = coordinates.lat;
                 site.longitude = coordinates.long;
                 if(site.latitude){
-                    db.updateSiteLocation(site.site_recid, coordinates.lat, coordinates.long)
+                    db.updateSiteLocation(site.site_recid, coordinates.lat.slice(0, 12), coordinates.long.slice(0, 12))
                         .catch(e => Log.error(`Site: ${site.site_recid} db update location failed`));
                 }
             }
