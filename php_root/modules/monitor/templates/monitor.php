a623f17c848f5c990a9b3d48e6cac7b1b71ed46a
Simon Howey <simon.howey@gmail.com>
2017-11-16 14:35:38 -0800

    js and css positioning for iframe

--- php_root/modules/monitor/templates/monitor.php
+++ php_root/modules/monitor/templates/monitor.php
@@ -13,14 +13,21 @@
 </style>
 
 <script type="text/javascript">
-    window.location = (""+window.location).replace(/#[A-Za-z0-9_]*$/,'')+"#menu_ul";
-    var iframeheight = window.innerHeight - 50;
-    document.getElementById('monitoringframe').style.height = iframeheight + "px";
-    console.log("windowheight: " + window.innerHeight);
+    function m8sready(){
+      window.location = (""+window.location).replace(/#[A-Za-z0-9_]*$/,'')+"#menu_ul";
+      var iframeheight = window.innerHeight - 50;
+      document.getElementById('monitoringframe').style.height = iframeheight + "px";
+    }
+    
+    if (document.readyState !== 'loading') {
+      m8sready()
+    } else {
+      document.addEventListener('DOMContentLoaded', m8sready)
+    }
 </script>
 
 <iframe src="/monitoringframe.html" title="Monitoring Iframe" id="monitoringframe">
-  <p>Your browser does not support iframes.</p>
+  <p>Iframes are required for monitoring. Your browser does not support iframes.</p>
 </iframe>
 
 
