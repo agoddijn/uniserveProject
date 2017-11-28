8a73cd357f4d241e60f5f083233c37f5de490637
Pyread <t8y9a@ugrad.cs.ubc.ca>
2017-11-27 18:25:15 -0800

    updated map resize functionality

--- services/Frontend/src/components/MapContainer.tsx
+++ services/Frontend/src/components/MapContainer.tsx
@@ -27,13 +27,15 @@ export class MapContainer extends React.Component<{ Sites: Site[], SetLayout: an
     componentWillReceiveProps(next: { Sites: Site[], SelectedSite: Site, SetSelectedSite: any, layoutupdate: boolean }) {
         this._renderMap(next.Sites, next.SelectedSite.site_recid, next.SetSelectedSite, next.SelectedSite, next.layoutupdate);
     }
+    private Map;
+    private updated:boolean = true;
     private _renderMap = (() => {
         let container = <div id={"newmap"} className={"container-inner"} style={{ height: "100%", width: "100%", position: "absolute" }}></div>
         let map = <div id={"newmapcontainer"} style={{ height: "95%", width: "100%" }}></div>;
         
-        let Map = withGoogleMap((props: { sites: Site[], markers: MarkerWrappers, SelectedSite: Site }) => {
+        this.Map = withGoogleMap((props: { sites: Site[], markers: MarkerWrappers, SelectedSite: Site }) => {
             return (
-                <GoogleMap defaultZoom={5} center={{ lat: Number(props.SelectedSite.latitude), lng: Number(props.SelectedSite.longitude) }}>
+                <GoogleMap defaultZoom={8} center={{ lat: Number(props.SelectedSite.latitude), lng: Number(props.SelectedSite.longitude) }}>
                     <MarkerClusterer
                         averageCenter
                         enableRetinaIcons
@@ -47,10 +49,11 @@ export class MapContainer extends React.Component<{ Sites: Site[], SetLayout: an
         });
         
         return function (sites: Site[], ID: number, SetSelectedSite: any, SelectedSite: Site, layout: boolean) {
-            if (layout) {
-                Map = withGoogleMap((props: { sites: Site[], markers: MarkerWrappers, SelectedSite: Site, this: any }) => {
+            let Map_new = this.Map
+            if (layout && !this.update) {
+                Map_new = withGoogleMap((props: { sites: Site[], markers: MarkerWrappers, SelectedSite: Site, this: any }) => {
                     return (
-                        <GoogleMap defaultZoom={5} center={{ lat: Number(props.SelectedSite.latitude), lng: Number(props.SelectedSite.longitude) }}>
+                        <GoogleMap defaultZoom={8} center={{ lat: Number(props.SelectedSite.latitude), lng: Number(props.SelectedSite.longitude) }}>
                             <MarkerClusterer
                                 averageCenter
                                 enableRetinaIcons
@@ -62,8 +65,11 @@ export class MapContainer extends React.Component<{ Sites: Site[], SetLayout: an
                         </GoogleMap>
                     )
                 });
+                this.update = true;
+            }else{
+                this.update = false;
             }
-            let map_ele: any = <Map sites={sites} SelectedSite={SelectedSite} containerElement={container} mapElement={map} markers={<MarkerWrappers Sites={sites} ClickedId={ID} SetSelectedSite={SetSelectedSite} />} />
+            let map_ele: any = <Map_new sites={sites} SelectedSite={SelectedSite} containerElement={container} mapElement={map} markers={<MarkerWrappers Sites={sites} ClickedId={ID} SetSelectedSite={SetSelectedSite} />} />
             this.setState({ Map: map_ele });
         }
     })();
